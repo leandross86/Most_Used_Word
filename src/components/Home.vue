d2e6f75ec8cada6a5d715940c807cf7875a61e67
leandross86 <leandroleandrossss@hotmail.com>
2020-06-21 13:21:13 -0300

    Iniciado projeto

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input 
+            label="Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple
+            chips
+            v-model="files"
+            @click:append-outer="processSubtitles" />
+
+       </v-form>
+      <div class="pills">
+          <Pill 
+          v-for="word in groupedWord" 
+          :key="word.name"
+          :name="word.name" 
+          :amount="word.amount"
+          />
+      </div>
+
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+
+    data: function() {
+        return {
+            files: [],
+            groupedWord: [
+                { name: 'i', amount: 1234},
+                { name: 'you', amount: 900},
+                { name: 'he', amount: 853},
+            ]
+        }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
+        }
+    }
+
+}
+</script>
+
+<style>
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
